84cc80ecf24f8fcd919ce29dacc3d76efa286767
ybdl <komeyang@drsvip.com>
2022-01-21 15:28:34 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-19 11:38:24
- * @LastEditTime: 2022-01-19 14:11:30
+ * @LastEditTime: 2022-01-21 14:58:22
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vueapp/src/App.vue
@@ -33,6 +33,7 @@
     <HelloWorld msg="Welcome to Your Vue.js App11"/>
   </div>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
